c513418815c4e048c726d49ba5430eeb888f2bf9
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-05 14:23:00 +0100

    Add berry API fetch

--- src/App.js
+++ src/App.js
@@ -4,9 +4,90 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [berry, setBerry] = useState("");
+  const [berryFirmness, setBerryFirmness] = useState("");
+  const [berryFlavour, setBerryFlavour] = useState("");
+
+  const [userBerry, setuserBerry] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/berry/1")
+      .then((response) => {
+        const apiBerry = {
+          category: "berry",
+          id: response.data.id,
+          name: response.data.name,
+        };
+        setBerry(apiBerry);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("https://pokeapi.co/api/v2/berry-firmness/1")
+      .then((response) => {
+        const apiBerryFirmness = {
+          category: "firmness",
+          id: response.data.id,
+          name: response.data.name,
+        };
+        setBerryFirmness(apiBerryFirmness);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("https://pokeapi.co/api/v2/berry-flavor/1")
+      .then((response) => {
+        const apiBerryFlavour = {
+          category: "flavour",
+          id: response.data.id,
+          name: response.data.name,
+        };
+        setBerryFlavour(apiBerryFlavour);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const submitHandler = (e) => {
+    const domUserBerry = document.getElementById("user-berry");
+
+    axios
+      .get(`https://pokeapi.co/api/v2/berry/${domUserBerry.value}`)
+      .then((response) => {
+        setuserBerry(response.data.name);
+      })
+      .catch((error) => {
+        setuserBerry("no berry available with the specified id.");
+      });
+
+    e.preventDefault();
+  };
+
   return (
     <>
-      <Card id="idcontent" title="titlecontent"></Card>
+      <h1>Welcome to Berryland!</h1>
+      <div className="card-container">
+        <Card berryInfo={berry}></Card>
+        <Card berryInfo={berryFirmness}></Card>
+        <Card berryInfo={berryFlavour}></Card>
+      </div>
+      <form onSubmit={submitHandler}>
+        <input
+          id="user-berry"
+          type="text"
+          placeholder="Enter a berry id..."
+        ></input>
+        <button type="submit">get!</button>
+      </form>
+      <div className="returned-berry">
+        <strong>Your berry:&nbsp;</strong> {userBerry}
+      </div>
     </>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
-export default function Card({ id, title }) {
+export default function Card({ berryInfo }) {
   return (
     <div className="card">
-      <div className="card-title">
-        {id} - {title}
+      <div className="card-title">{berryInfo.category}</div>
+      <div className="card-content">
+        <div className="card-content-item">
+          <strong>Id: </strong> {berryInfo.id}
+        </div>
+        <div className="card-content-item">
+          <strong>Name: </strong>
+          {berryInfo.name}
+        </div>
       </div>
     </div>
   );
